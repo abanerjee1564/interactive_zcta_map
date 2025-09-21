780cf412ca242e7e121cbc2932bbf9ee7a3cc28a
Akash Banerjee <akashbanerjee@Akashs-MacBook-Pro.local>
2025-09-20 23:59:06 -0400

    decreased precision

--- convert_zcta_shp_to_geojson.R
+++ convert_zcta_shp_to_geojson.R
@@ -7,7 +7,7 @@ shp <- st_read("/Users/akashbanerjee/Downloads/nhgis0084_shape/nhgis0084_shapefi
 shp_ll <- st_transform(shp_simp, 4326)
 
 # Simplify geometry for web (tolerance in map units; tune as needed)
-shp_simp_ll <- st_simplify(shp_ll, dTolerance = 300) # reduce complexity (tolerance depends on unit)
+shp_simp_ll <- st_simplify(shp_ll, dTolerance = 3000)
 
 # Make sure GEOID20 is a string (important)
 shp_simp_ll$GEOID20 <- as.character(shp_simp_ll$GEOID20)
