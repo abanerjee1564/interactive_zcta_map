53ac1f2216935d2bf2631a490f6c192c5be89c38
Akash Banerjee <akashbanerjee@Akashs-MacBook-Pro.local>
2025-09-20 23:29:43 -0400

    try reprojecting geojson into lat/long

--- convert_zcta_shp_to_geojson.R
+++ convert_zcta_shp_to_geojson.R
@@ -3,11 +3,16 @@ library(sf)
 # adjust path to your shapefile folder
 shp <- st_read("/Users/akashbanerjee/Downloads/nhgis0084_shape/nhgis0084_shapefile_tl2020_us_zcta_2020/US_zcta_2020.shp")   # reads shapefile
 
-# Optional: simplify geometry for web (tolerance in map units; tune as needed)
-shp_simp <- st_simplify(shp, dTolerance = 300) # reduce complexity (tolerance depends on unit)
+# reproject to WGS84 lon/lat
+shp_ll <- st_transform(shp_simp, 4326)
+
+# Simplify geometry for web (tolerance in map units; tune as needed)
+shp_simp_ll <- st_simplify(shp_ll, dTolerance = 300) # reduce complexity (tolerance depends on unit)
 
 # Make sure GEOID20 is a string (important)
-shp_simp$GEOID20 <- as.character(shp_simp$GEOID20)
+shp_simp_ll$GEOID20 <- as.character(shp_simp_ll$GEOID20)
 
 # Write GeoJSON (UTF-8)
-st_write(shp_simp, "/Users/akashbanerjee/Desktop/1_Projects/interactive_zcta_map/zcta.geojson", driver = "GeoJSON", delete_dsn = TRUE)
+st_write(shp_simp_ll, 
+         "/Users/akashbanerjee/Desktop/1_Projects/interactive_zcta_map/zcta.geojson", 
+         driver = "GeoJSON", delete_dsn = TRUE)
