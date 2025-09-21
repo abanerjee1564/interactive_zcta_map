bbc829b4a146a8996fc235b8588c544c2677ed0a
Akash Banerjee <akashbanerjee@Akashs-MacBook-Pro.local>
2025-09-20 23:16:40 -0400

    simplified geojson file

--- convert_zcta_shp_to_geojson.R
+++ convert_zcta_shp_to_geojson.R
@@ -1,13 +1,13 @@
 library(sf)
 
 # adjust path to your shapefile folder
-shp <- st_read("/Users/akashbanerjee/Desktop/1_Projects/interactive_map/US_zcta_2020.shp")   # reads shapefile
+shp <- st_read("/Users/akashbanerjee/Downloads/nhgis0084_shape/nhgis0084_shapefile_tl2020_us_zcta_2020/US_zcta_2020.shp")   # reads shapefile
 
 # Optional: simplify geometry for web (tolerance in map units; tune as needed)
-shp_simp <- st_simplify(shp, dTolerance = 100) # reduce complexity (tolerance depends on unit)
+shp_simp <- st_simplify(shp, dTolerance = 300) # reduce complexity (tolerance depends on unit)
 
 # Make sure GEOID20 is a string (important)
 shp_simp$GEOID20 <- as.character(shp_simp$GEOID20)
 
 # Write GeoJSON (UTF-8)
-st_write(shp_simp, "zcta.geojson", driver = "GeoJSON", delete_dsn = TRUE)
+st_write(shp_simp, "/Users/akashbanerjee/Desktop/1_Projects/interactive_zcta_map/zcta.geojson", driver = "GeoJSON", delete_dsn = TRUE)
